3fafd5d92ff6e2f25ff91e1345b91f9ab8cca0a4
Strikker0 <97169587+Strikker0@users.noreply.github.com>
2023-04-03 22:22:49 -0700

    Add files via upload

---
+++ event_junctions/src/VideoBanner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './VideoBanner.css';
+
+function VideoBanner() {
+    return (
+        <div className="videoBanner">
+            <div className="videoBanner__info">
+                <h1>Hire the best event junction for any job, online.</h1>
+                <h4>
+                    Millions of people use eventjunction.com to turn their ideas into reality.
+                </h4>
+                <button className="videoBanner__info--first">
+                    Hire a event junction
+                </button>
+                <button className="videoBanner__info--second">
+                    Earn Money event junction
+                </button>
+            </div>
+
+        </div>
+    )
+}
+
+export default VideoBanner
